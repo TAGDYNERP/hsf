61ebc46253d38101ce29bab1f4302f667956050a
TAGDYNERP <erp@tagdyn.com>
2026-04-02 17:01:16 +0400

    fix: Change item group filter from 'Facilities' to 'Facility Items'

--- hsf/permissions.py
+++ hsf/permissions.py
@@ -2,7 +2,7 @@ import frappe
 
 
 def item_query_conditions(user):
-	"""HSF Users can only see items in the 'Facilities' item group."""
+	"""HSF Users can only see items in the 'Facility Items' item group."""
 	if not user:
 		user = frappe.session.user
 
@@ -10,7 +10,7 @@ def item_query_conditions(user):
 		return ""
 
 	if "HSF User" in frappe.get_roles(user) and "Stock User" not in frappe.get_roles(user):
-		return "`tabItem`.`item_group` = 'Facilities'"
+		return "`tabItem`.`item_group` = 'Facility Items'"
 
 	return ""
 
